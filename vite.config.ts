81168e6c392cd35221cddc57edd98568c133e7f5
FiezDev <79656850+FiezDev@users.noreply.github.com>
2024-12-26 18:46:00 +0700

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
-      "@/*": "./src/*",
+      "@": "/src",
     },
   },
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     target: 'esnext',
-  }
+  },
 });
